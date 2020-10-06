01ed41ea466a85c6a28ace35055f1eac0a350ad1
Timothy Liu <timothy.b.liu@gmail.com>
2020-10-05 20:49:42 -0700

    Add account positions fetching

--- rebalance.go
+++ rebalance.go
@@ -30,6 +30,66 @@ func getPorfolioAllocation() map[string]float32 {
 	return allocation;
 }
 
+func genAccountInfo(
+	endpoint string,
+	api_key string,
+	api_secret string,
+) (*alpaca.Account, string) {
+	client := &http.Client {};
+	req, _ := http.NewRequest("GET", endpoint + "/account", nil);
+	req.Header.Add("APCA-API-KEY-ID", api_key);
+	req.Header.Add("APCA-API-SECRET-KEY", api_secret);
+	resp, err := client.Do(req);
+
+	if err != nil {
+		log.Fatal(err);
+		return nil, fmt.Sprintf("Error: %s", err.Error());
+	}
+	
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		log.Fatal("Error: Response code was %d", resp.StatusCode);
+		return nil, fmt.Sprintf("Error: Status code was %d", resp.StatusCode);
+	}
+	
+	defer resp.Body.Close();
+	body, _ := ioutil.ReadAll(resp.Body);
+	account := alpaca.Account {};
+	json.Unmarshal(body, &account);
+
+	return &account, "";
+}
+
+func genAccountPositions(
+	endpoint string,
+	api_key string,
+	api_secret string,
+	desiredAllocation map[string]float32,
+) ([]*alpaca.Position, string) {
+	client := &http.Client {};
+	req, _ := http.NewRequest("GET", endpoint + "/positions", nil);
+	req.Header.Add("APCA-API-KEY-ID", api_key);
+	req.Header.Add("APCA-API-SECRET-KEY", api_secret);
+	resp, err := client.Do(req);
+
+	if err != nil {
+		log.Fatal(err);
+		return nil, fmt.Sprintf("Error: %s", err.Error());
+	}
+
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		log.Fatal("Error: Respones code was %d", resp.StatusCode);
+		return nil, fmt.Sprintf("Error: Status code was %d", resp.StatusCode);
+	}
+
+	defer resp.Body.Close();
+
+	body, _ := ioutil.ReadAll(resp.Body);
+	positions := make([]*alpaca.Position, len(desiredAllocation));
+	json.Unmarshal(body, &positions);
+
+	return positions, "";
+}
+
 func rebalance(dryRun bool) (int, string) {
 	desiredAllocation := getPorfolioAllocation();
 
@@ -48,46 +108,31 @@ func rebalance(dryRun bool) (int, string) {
 	}
 
 	// Get account equity
-	client := &http.Client {};
-	req, _ := http.NewRequest("GET", ENDPOINT + "/account", nil);
-	req.Header.Add("APCA-API-KEY-ID", API_KEY);
-	req.Header.Add("APCA-API-SECRET-KEY", API_SECRET);
-	resp, err := client.Do(req);
+	account, err := genAccountInfo(ENDPOINT, API_KEY, API_SECRET);
+	if err != "" {
+		return 1, err;
+	}
 
-	if err != nil {
-		log.Fatal(err);
-		return 1, fmt.Sprintf("Error: %s", err.Error());
-	}
-	
-	if resp.StatusCode < 200 || resp.StatusCode > 299 {
-		log.Fatal("Error: Response code was %d", resp.StatusCode);
-		return 1, fmt.Sprintf("Error: Status code was %d", resp.StatusCode);
-	}
-	
-	defer resp.Body.Close();
-	body, _ := ioutil.ReadAll(resp.Body);
-	account := alpaca.Account {};
-	json.Unmarshal(body, &account);
 	accountEquity := account.Equity;
+
+	fmt.Println("Account equity", accountEquity);
 
 	// Iterate through portfolio to see if desired vs. actual allocation
 	// deviates by 5% or more
-	actualAllocation := make(map[string]float32);
-	req, _ = http.NewRequest("GET", ENDPOINT + "/positions", nil);
-	req.Header.Add("APCA-API-KEY-ID", API_KEY);
-	req.Header.Add("APCA-API-SECRET-KEY", API_SECRET);
-	resp, err = client.Do(req);
+	// actualAllocation := make(map[string]float32);
 
-	if err != nil {
-		log.Fatal(err);
+	positions, err := genAccountPositions(ENDPOINT, API_KEY, API_SECRET, desiredAllocation);
+	if err != "" {
+		return 1, err;
 	}
+	fmt.Println(positions);
+	return 0, "";
 
-	var positions [len(desiredAllocation)] alpaca.Position;
+	// for ticker, allocationPercentage := range desiredAllocation {
 
-	for ticker, allocationPercentage := range desiredAllocation {
-
-	}
+	// }
 }
 
 func main() {
+	rebalance(true);
 }
